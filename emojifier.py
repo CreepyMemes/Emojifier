4407f6111b193f87019ef260d70d5a512affaaae
CreepyMemes <61922260+CreepyMemes@users.noreply.github.com>
2022-02-06 14:02:54 +0100

    Update emojifier.py

--- emojifier.py
+++ emojifier.py
@@ -1,23 +1,20 @@
 from PIL import Image
 from math import sqrt
 from glob import glob
-import os
 
-def progress(n, l, img):
-    os.system('cls' if os.name == 'nt' else 'clear') #clears the output
-    n=round((n+1)/l*50.0)
-    print(f"Loading image: {img}")
-    print(f"Progress: [{'#'*n}{'-'*(50-n)}]  {n*2}%")
+#prints a simple one line progress bar (argument [0.0 -> 1.0])
+def progress(progress): #progress [0.0 -> 1.0]
+    barLength = 50 
+    pos = progress*barLength
+    print( f"Progress: [{'#'*int(pos)}{'-'*(barLength-int(pos))}]  {int(progress * 100)} %\r", end='') 
 
+#returns the distance between two 3D points (used to get the most similar emoji color in the <os>.txt file to current pixel)
 def dist(x,y,z,x1,y1,z1): 
     return sqrt( (x-x1)**2 + (y-y1)**2 + (z-z1)**2 )
     
-def getName(s): 
-    return s.split(".")[0]
-
 def main():
     #Prints all the files available in the folder images and asks for input which one to convert
-    path = "images/"
+    path     = "images/"
     os_emoji = "windows"
     for idx, file in enumerate(glob(f"{path}*")):
         print(f"{idx}. {file[7:]}")   
@@ -25,15 +22,15 @@ def main():
 
     #Loads the image and resizes it according to input (input is width "col")
     img_raw = Image.open(f"{path}{name}").convert("RGB")
-    col = int(input("Input image size: "))
+    col     = int(input("Input image size: "))
     img_raw = img_raw.resize( (col, int(img_raw.size[1] / (img_raw.size[0] / col))) )
     #loads image pixels to pix and creates a 2d list img containing pixels
-    pix = img_raw.load()
-    width = img_raw.size[1]
-    img = [[pix[x, y] for x in range(img_raw.size[0])] for y in range(width)]
+    pix     = img_raw.load()
+    width   = img_raw.size[1]
+    img     = [[pix[x, y] for x in range(img_raw.size[0])] for y in range(width)]
 
     #Opens the html result file and prints the heading for "utf-8" and "black background"
-    out = open(f"{getName(name)}.html", "w")
+    out = open(f"{name.split('.')[0]}.html", "w")
     out.write('<meta charset="UTF-8">\n')
     out.write('<body style="background-color:black;">\n')
 
@@ -43,8 +40,9 @@ def main():
     rgb = list(data.values()) #turns rgb values in a list from dictionary data
 
     #Iterates through the image pixel by pixel from top to bottom
+    print(f"\nConverting image: {name}")
     for count, row in enumerate(img):
-        progress(count, width, name)
+        progress((count+1)/len(row))
         for item in row:
             distances = [dist(item[0], item[1], item[2], rgb[i][0][0], rgb[i][0][1], rgb[i][0][2]) for i in range(len(rgb))]
             out.write(rgb[distances.index(min(distances))][1])
