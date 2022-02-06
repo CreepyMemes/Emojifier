24a2f742208114dd762d91c99fb0f629f76a2233
CreepyMemes <61922260+CreepyMemes@users.noreply.github.com>
2022-02-06 15:29:41 +0100

    Update emojifier.py

--- emojifier.py
+++ emojifier.py
@@ -22,7 +22,7 @@ def main():
 
     #Loads the image and resizes it according to input (input is width "col")
     img_raw = Image.open(f"{path}{name}").convert("RGB")
-    width   = int(input("Input image size: "))
+    width     = int(input("Input image size: "))
     img_raw = img_raw.resize( (width, int(img_raw.size[1] / (img_raw.size[0] / width))) )
     #loads image pixels to pix and creates a 2d list img containing pixels
     pix     = img_raw.load()
@@ -30,24 +30,24 @@ def main():
     height  = img_raw.size[1]
     img     = [[pix[x, y] for x in range(width)] for y in range(height)]
 
-    #Opens the html result file and prints the heading for "utf-8" and "black background"
-    out = open(f"{name.split('.')[0]}.html", "w")
-    out.write('<meta charset="UTF-8">\n')
-    out.write('<body style="background-color:black;">\n')
-
     #Saves data from file in a dictionary {"name.png", [r,g,b], "&#xUNICODE"}
     with open(f"data\{os_emoji}.txt", "r") as inp:
         data = {line.split(":")[0] : (list(map(int, line.split(":")[1].split(","))), line.split(":")[2][:-1]) for line in inp}
     rgb = list(data.values()) #turns rgb values in a list from dictionary data
 
-    #Iterates through the image pixel by pixel from top to bottom
-    print(f"\nConverting image: {name}")
-    for count, row in enumerate(img):
-        progress((count+1)/height)
-        for item in row:
-            distances = [dist(item[0], item[1], item[2], rgb[i][0][0], rgb[i][0][1], rgb[i][0][2]) for i in range(len(rgb))]
-            out.write(rgb[distances.index(min(distances))][1])
-        out.write("<br>")
+    #Opens the html result file and prints the heading for "utf-8" and "black background"
+    with open(f"{name.split('.')[0]}.html", "w") as out:
+        out.write('<meta charset="UTF-8">\n')
+        out.write('<body style="background-color:black;">\n')
+        print(f"\nConverting image: {name}")
+
+        #Iterates through the image pixel by pixel from top to bottom
+        for count, row in enumerate(img):
+            progress((count+1)/height)
+            for item in row:
+                distances = [dist(item[0], item[1], item[2], rgb[i][0][0], rgb[i][0][1], rgb[i][0][2]) for i in range(len(rgb))]
+                out.write(rgb[distances.index(min(distances))][1])
+            out.write("<br>")
     print("\nDone")
 
 if __name__ == '__main__':
